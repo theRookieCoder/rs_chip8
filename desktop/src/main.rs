702a2654a7c87d2396b373bb17e48f742d259fd9
Ilesh Thiada <ileshkt@gmail.com>
2025-02-13 23:45:48 +0530

    Fix window resize/move on Windows freezing emulation

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,6 +1,12 @@
+use parking_lot::Mutex;
 use rand::Rng;
 use rs_chip8_core::{DISPLAY_HEIGHT, DISPLAY_WIDTH, EmulationSystem, MachineState};
-use sdl3::{event::Event, keyboard::Scancode, pixels::Color, rect::Point};
+use sdl3::{
+    event::{Event, WindowEvent},
+    keyboard::Scancode,
+    pixels::Color,
+    rect::Point,
+};
 use std::{
     ffi::OsStr,
     path::PathBuf,
@@ -36,6 +42,7 @@ const KEYMAP: [Scancode; 16] = [
 #[derive(Debug, thiserror::Error)]
 #[error(transparent)]
 enum Error {
+    Sdl(#[from] sdl3::Error),
     Core(#[from] rs_chip8_core::Error),
     #[error("One argument required")]
     Argument,
@@ -53,34 +60,6 @@ fn main() -> ExitCode {
 }
 
 fn actual_main() -> Result<(), Error> {
-    // Initialise SDL
-    let sdl_context = sdl3::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-    // let audio_subsystem = sdl_context.audio().unwrap();
-    let mut event_pump = sdl_context.event_pump().unwrap();
-
-    let window = video_subsystem
-        .window("rs_chip8", 1280, 640)
-        .position_centered()
-        .resizable()
-        .build()
-        .unwrap();
-
-    // Set logical resolution to the DISPLAY_WIDTH x DISPLAY_HEIGHT
-    let mut canvas = window.into_canvas();
-    canvas
-        .set_logical_size(
-            DISPLAY_WIDTH as u32,
-            DISPLAY_HEIGHT as u32,
-            sdl3::sys::render::SDL_RendererLogicalPresentation::LETTERBOX,
-        )
-        .unwrap();
-
-    // Clear the screen
-    canvas.set_draw_color(OFF_COLOUR);
-    canvas.clear();
-    canvas.present();
-
     // Open and read the program
     let rom_filepath = PathBuf::from(std::env::args().nth(1).ok_or(Error::Argument)?);
     let program = std::fs::read(&rom_filepath)?;
@@ -95,44 +74,59 @@ fn actual_main() -> Result<(), Error> {
         });
     machine_state.load_default_font();
     machine_state.load_program(&program);
+    let machine_state = Mutex::new(machine_state);
+
+    // Initialise SDL
+    let sdl_context = sdl3::init()?;
+    let video_subsystem = sdl_context.video()?;
+    let _audio_subsystem = sdl_context.audio()?;
+    let event_subsystem = sdl_context.event()?;
+    let mut event_pump = sdl_context.event_pump()?;
+
+    let window = match video_subsystem
+        .window("rs_chip8", 1280, 640)
+        .position_centered()
+        .resizable()
+        .build()
+    {
+        Ok(window) => window,
+        Err(err) => match err {
+            sdl3::video::WindowBuildError::SdlError(err) => return Err(err.into()),
+            _ => panic!(
+                "Expected window dimensions and title to be valid, but {}",
+                err
+            ),
+        },
+    };
+
+    // Set logical resolution to the DISPLAY_WIDTH x DISPLAY_HEIGHT
+    let canvas = Mutex::new(window.into_canvas());
+    if let Err(err) = canvas.lock().set_logical_size(
+        DISPLAY_WIDTH as u32,
+        DISPLAY_HEIGHT as u32,
+        sdl3::sys::render::SDL_RendererLogicalPresentation::LETTERBOX,
+    ) {
+        if let sdl3::IntegerOrSdlError::SdlError(err) = err {
+            return Err(err.into());
+        } else {
+            panic!("Expected display height and width to be valid");
+        }
+    }
 
     // Time period of 60 Hz
     let time_period = Duration::from_secs(1) / 60;
-    let mut prev_tick = Instant::now();
+    let prev_tick = Mutex::new(Instant::now());
 
-    let mut held_keys: u16 = 0;
-    let mut rng = rand::rng();
+    let held_keys = Mutex::new(0_u16);
+    let rng = Mutex::new(rand::rng());
 
-    loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. } => return Ok(()),
-                Event::KeyDown {
-                    scancode: Some(scancode),
-                    ..
-                } => {
-                    if let Some(i) = KEYMAP.iter().position(|key| key == &scancode) {
-                        held_keys |= 0b1 << i;
-                    }
-                }
-                Event::KeyUp {
-                    scancode: Some(scancode),
-                    ..
-                } => {
-                    if let Some(i) = KEYMAP.iter().position(|key| key == &scancode) {
-                        held_keys -= 0b1 << i;
-                    }
-                }
-                _ => (),
-            }
-        }
+    struct ExecutionErrorEvent(Error);
+    event_subsystem.register_custom_event::<ExecutionErrorEvent>()?;
 
-        let delta = prev_tick.elapsed();
-        if delta < time_period {
-            continue;
-        } else {
-            prev_tick += time_period;
-        }
+    let execution_loop = || -> Result<(), Error> {
+        let mut machine_state = machine_state.lock();
+        let held_keys = held_keys.lock();
+        let mut rng = rng.lock();
 
         machine_state.tick_timer();
 
@@ -143,8 +137,10 @@ fn actual_main() -> Result<(), Error> {
         }
 
         for _ in 0..=INSTR_PER_FRAME {
-            machine_state.tick(|| held_keys, || rng.random())?;
+            machine_state.tick(|| *held_keys, || rng.random())?;
         }
+
+        let mut canvas = canvas.lock();
 
         canvas.set_draw_color(OFF_COLOUR);
         canvas.clear();
@@ -153,13 +149,69 @@ fn actual_main() -> Result<(), Error> {
         for y in 0..DISPLAY_HEIGHT {
             for x in 0..DISPLAY_WIDTH {
                 if machine_state.display_buffer[x][y] {
-                    canvas.draw_point(Point::new(x as i32, y as i32)).unwrap();
+                    canvas.draw_point(Point::new(x as i32, y as i32))?;
                 }
             }
         }
+
         canvas.present();
 
-        // After this, busy wait for 1 ms - processing time
-        sleep(time_period - Duration::from_millis(1));
+        Ok(())
+    };
+
+    let _window_update_eventwatch = event_subsystem.add_event_watch(|event| {
+        if let Event::Window {
+            win_event: WindowEvent::Exposed,
+            ..
+        } = event
+        {
+            let delta = prev_tick.lock().elapsed();
+            if delta > time_period {
+                *prev_tick.lock() += time_period;
+                if let Err(err) = execution_loop() {
+                    event_subsystem
+                        .push_custom_event(ExecutionErrorEvent(err))
+                        .expect("Custom event was not registered");
+                }
+            }
+        }
+    });
+
+    loop {
+        let delta = prev_tick.lock().elapsed();
+        if delta < time_period {
+            sleep(time_period - delta);
+            continue;
+        }
+        *prev_tick.lock() += time_period;
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } => return Ok(()),
+                Event::KeyDown {
+                    scancode: Some(scancode),
+                    ..
+                } => {
+                    if let Some(i) = KEYMAP.iter().position(|key| key == &scancode) {
+                        *held_keys.lock() |= 0b1 << i;
+                    }
+                }
+                Event::KeyUp {
+                    scancode: Some(scancode),
+                    ..
+                } => {
+                    if let Some(i) = KEYMAP.iter().position(|key| key == &scancode) {
+                        *held_keys.lock() -= 0b1 << i;
+                    }
+                }
+                _ => {
+                    if let Some(event) = event.as_user_event_type::<ExecutionErrorEvent>() {
+                        return Err(event.0);
+                    }
+                }
+            }
+        }
+
+        execution_loop()?;
     }
 }
